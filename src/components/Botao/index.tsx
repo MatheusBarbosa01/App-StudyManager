045989ab09021a130eacbb835e11ac64f63de0fb
Matheus Barbosa <matheus.barbosa@fomento.pi.gov.br>
2024-04-30 13:49:06 -0300

    clock missing

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from "react";
+import style from './Botao.module.scss'
+
+class Botao extends React.Component<{children: ReactNode, type?: "button" | "submit" | "reset" | undefined}>{
+    render(){
+        const{type = "button" } = this.props;
+        return(
+            <button type={type}className={style.botao}>
+                {this.props.children}
+            </button>
+        )
+    }
+}
+
+export default Botao;
---
+++ src/components/Cronometro/Relogio/index.tsx
@@ -0,0 +1,20 @@
+import style from './Relogio.module.scss'
+
+interface Props{
+    tempo:number | undefined
+}
+
+export default function Relogio({tempo = 0}:Props) {
+    const minutos = Math.floor(tempo/60);
+    const segundos = tempo%60
+    return (
+        <>
+            <span className={style.relogioNumero}>0</span>
+            <span className={style.relogioNumero}>0</span>
+            <span className={style.relogioDivisao}>:</span>
+            <span className={style.relogioNumero}>0</span>
+            <span className={style.relogioNumero}>0</span>
+
+        </>
+    )
+}
